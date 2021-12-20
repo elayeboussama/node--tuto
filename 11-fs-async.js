70ef3eae91448bd3808c622c72b18a2b003c2e6b
elayeboussama <elayeb.oussama2020@gmail.com>
2021-12-20 07:39:53 +0100

    first commit

---
+++ 11-fs-async.js
@@ -0,0 +1,24 @@
+const {readFile,writeFile} = require('fs')
+
+readFile('./content/first.txt','utf8', (err, result)=>{
+    if(err){
+        console.log(err)
+        return
+    }
+    const first = result
+    readFile('./content/second.txt','utf8',(err,result)=>{
+        if(err){
+            console.log(err)
+        }
+        const second = result
+        writeFile('./content/result-async.txt',`here is the result: ${first}, ${second}`,
+        (err,result)=>{
+            if(err){
+                console.log(err)
+            }
+            console.log(result)
+        }
+        )
+
+    })
+})
---
+++ 3-modules.js
@@ -0,0 +1,13 @@
+//Modules
+const names = require('./4-names')
+// const {john, peter} = require('./4-names')
+const sayHi =require('./5-utils')
+const data = require('./6-alternative-flavor')
+require('./7-mind-grenade')
+
+
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
+
+
